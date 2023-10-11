7cbf36f7f6fc9cd4e6e0271722c0b089ed1aa3e2
bruno-pisciotta281 <bruno.pisciotta43@gmail.com>
2023-10-11 10:48:27 -0300

    Correção do end-point na conexão

--- api/conn.php
+++ api/conn.php
@@ -1,18 +1,16 @@
 <?php
-// Configurações de conexão com o banco de dados PostgreSQL
-$host = 'ep-frosty-union-26727263.us-east-1.postgres.vercel-storage.com';
-$port = '5432';
-$dbname = 'verceldb';
-$user = 'default';
-$password = 'UMFsvA7JpZy9';
+$host = 'ep-frosty-union-26727263-pooler.us-east-1.postgres.vercel-storage.com?options=endpoint%3Dep-frosty-union-26727263-pooler';
+// Substitua <endpoint-id> pelo seu ID de ponto de extremidade
+$port = '5432'; // Porta padrão do PostgreSQL
+$dbname = 'verceldb'; // Nome do seu banco de dados
+$user = 'default'; // Nome de usuário do banco de dados
+$password = 'UMFsvA7JpZy9'; // Senha do banco de dados
 
-// Conecta ao banco de dados PostgreSQL
-$pgsql_conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+// Configuração da conexão com SSL
+$pgsql_conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
 // Verifica a conexão com o banco de dados
 if (!$pgsql_conn) {
     die('Erro na conexão com o banco de dados: ' . pg_last_error());
 }
-
-// Resto do código para receber e inserir os dados aqui...
 ?>
